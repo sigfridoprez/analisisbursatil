b31f4e19a19cd34e1f99f3558bdccbbcc4bff91c
sigfrido.perez.coapango <sigfrido.perez.coapango@37014470-aa8e-11dd-a714-1752b6e4c161>
2009-02-18 06:22:50 +0000

    cambios

--- trunk/JavaApplication1/src/javaapplication1/Main.java
+++ trunk/JavaApplication1/src/javaapplication1/Main.java
@@ -2,14 +2,16 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package javaapplication1;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Properties;
 import javax.naming.Context;
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.sql.DataSource;
+
 
 /**
  *
@@ -22,12 +24,34 @@ public class Main {
      */
     public static void main(String[] args) throws Exception {
         System.out.println("Hola mundo");
-        Properties  pro = new Properties();
+        Properties pro = new Properties();
+        String foo = "Not Connected";
+        int bar = -1;
+
+
+        pro.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.enterprise.naming.SerialInitContextFactory");
+        pro.put(Context.PROVIDER_URL, "iiop://127.0.0.1:3700");
+
+        //Context ctx = new InitialContext(pro);
+        Context ctx = new InitialContext();
+
+        System.out.println("ctx::" + ctx);
+        DataSource ds = (DataSource) ctx.lookup("jdbc/JNDI_DERBY");
+        System.out.println("datasource::"+ds);
         
-        pro.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.tomcat.dbcp.dbcp.BasicDataSourceFactory");
-        Context ctx = new InitialContext(pro);
-        
-        DataSource ds  = (DataSource)ctx.lookup("java:comp/env/jdbc/JNDI_DERBY");
-        System.out.println("ds::"+ds);
+        Connection conn = ds.getConnection();
+
+        foo = "Got Connection " + conn.toString();
+        Statement stmt = conn.createStatement();
+        ResultSet rst =
+                stmt.executeQuery(
+                "select hotelid,hotelname from TRAVEL.HOTEL");
+        if (rst.next()) {
+            foo = rst.getString(2);
+            bar = rst.getInt(1);
+        }
+        conn.close();
+        System.out.println("foo:::"+foo);
+        System.out.println("bar:::"+bar);
     }
 }
