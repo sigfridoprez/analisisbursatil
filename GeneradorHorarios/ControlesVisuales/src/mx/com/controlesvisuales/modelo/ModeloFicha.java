1a8ab9f3a2e40684f77726f04803816910aa99b8
Said ÖZCAN <said@ozcan.co>
2009-06-15 21:17:10 +0000

    Pruebas Controles Visuales Ver. 0.0.1

---
+++ GeneradorHorarios/ControlesVisuales/src/mx/com/controlesvisuales/modelo/ModeloFicha.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mx.com.controlesvisuales.modelo;
+
+/**
+ *
+ * @author sigfrido
+ */
+public interface ModeloFicha {
+
+    public String getStrMensaje();
+
+    public void setStrMensaje(String strMensaje);
+
+}
